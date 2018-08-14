8f4499cfc808f0d3498a4836dc209bf8afd26355
Omar Faroque <faroque.omar@outlook.com>
2018-08-13 22:18:54 -0400

    page 21 example

---
+++ Examples/src/org/book/Example.java
@@ -0,0 +1,14 @@
+package org.book;
+
+/***
+ * Page 21
+ * @author Admin
+ *
+ */
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println("This is simple program");
+	}
+	
+}
