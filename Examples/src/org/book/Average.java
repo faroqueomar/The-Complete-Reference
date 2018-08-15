8306949daed3e8cbc00fc4b619fd14f9fc03c4c8
Omar Faroque <faroque.omar@outlook.com>
2018-08-15 09:42:36 -0400

    Average Page 50

---
+++ Examples/src/org/book/Average.java
@@ -0,0 +1,18 @@
+package org.book;
+/***
+ * page 50
+ * @author Admin
+ *
+ */
+public class Average {
+
+	public static void main(String[] args) {
+		double nums[] = {10.1,11.2,12.3,13.4,14.5};
+		double result = 0;
+		int i;
+		
+		for(i=0;i<5;i++)
+			result = result +nums[i];
+		System.out.println("Average is " + result / 5);
+	}
+}
