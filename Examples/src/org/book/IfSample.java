8cadd1f77aeae5ff371df64712a338afe60b9edb
Omar Faroque <faroque.omar@outlook.com>
2018-08-14 20:50:13 -0400

    IfSample Page 27

---
+++ Examples/src/org/book/IfSample.java
@@ -0,0 +1,25 @@
+package org.book;
+
+/***
+ * Page 27
+ * @author Admin
+ *
+ */
+public class IfSample {
+
+	public static void main(String[] args) {
+		int x, y;
+		
+			x = 10;
+			y = 20;
+			if(x < y) System.out.println("x is less than y");
+			
+			x = x * 2;
+			if(x == y) System.out.println("x now equel to y");
+			
+			x = x * 2;
+			if(x > y) System.out.println("x now greater than y");
+			
+			if (x == y) System.out.println("you won't see this");
+	}
+}
