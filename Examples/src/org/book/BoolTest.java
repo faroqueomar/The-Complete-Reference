ef30ac0db1903ccd01d2bf5542e32cee010b5993
Omar Faroque <faroque.omar@outlook.com>
2018-08-15 08:20:59 -0400

    No change

--- Examples/src/org/book/BoolTest.java
+++ Examples/src/org/book/BoolTest.java
@@ -15,10 +15,12 @@ public class BoolTest {
 		b = true;
 		System.out.println("b is " + b);
 		
-		if(b) System.out.println("This is executed.");
+		if(b) 
+		System.out.println("This is executed.");
 		
 		b = false;
-		if(b) System.out.println("This is not executed.");
+		if(b)
+		System.out.println("This is not executed.");
 		
 		System.out.println("10 > 9 is " + (10 > 9));
 	}
