afd992d01d2a14ee0f2c4e120ff306cda4f15b24
Omar Faroque <faroque.omar@outlook.com>
2018-08-15 08:35:40 -0400

    DynInit Page 42

---
+++ Examples/src/org/book/DynInit.java
@@ -0,0 +1,16 @@
+package org.book;
+/***
+ * page 42
+ * @author Admin
+ *
+ */
+public class DynInit {
+
+	public static void main(String[] args) {
+		double a = 3.0, b = 4.0;
+		
+		double c = Math.sqrt(a * a + b * b);
+		
+		System.out.println("Hypotenuse is " + c);
+	}
+}
