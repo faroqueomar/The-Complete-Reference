c76deafeef8f1948193c182f7bbc10a2d14aced2
Omar Faroque <faroque.omar@outlook.com>
2018-08-17 10:41:23 -0400

    While Page 85

---
+++ Examples/src/org/book/While.java
@@ -0,0 +1,17 @@
+package org.book;
+/***
+ * page 85
+ * @author Admin
+ *
+ */
+public class While {
+
+	public static void main(String[] args) {
+		int n = 10;
+		
+		while(n > 0) {
+			System.out.println("tick " + n);
+			n--;
+		}
+	}
+}
