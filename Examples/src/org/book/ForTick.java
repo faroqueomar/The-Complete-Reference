fc8ee03821482bc14dc81cc0f7d8dc7351f75f8d
Omar Faroque <faroque.omar@outlook.com>
2018-08-17 10:39:21 -0400

    ForTick Page 89

---
+++ Examples/src/org/book/ForTick.java
@@ -0,0 +1,20 @@
+package org.book;
+/***
+ * page 89
+ * @author Admin
+ *
+ */
+public class ForTick {
+
+	public static void main(String[] args) {
+		int n;
+		for(n=10; n>0; n--)
+		System.out.println("tick " + n);
+		
+	/*	// here, n is declared inside of the for loop
+		for(int n=10; n>0; n--)
+		System.out.println("tick " + n);
+		
+	*/
+	}
+}
