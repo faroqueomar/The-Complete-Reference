5f0a28e9248a2e3d5f4e243eb17111bf71fbf49e
Omar Faroque <faroque.omar@outlook.com>
2018-08-19 18:39:13 -0400

    Ternary Page 74

---
+++ Examples/src/org/book/Ternary.java
@@ -0,0 +1,24 @@
+package org.book;
+/***
+ * page 74
+ * @author Admin
+ *
+ */
+public class Ternary {
+
+	public static void main(String[] args) {
+		int i, k;
+		
+		i = 10;
+		k = i < 0 ? -i : i;
+		
+		System.out.print("Absolute value of ");
+		System.out.println(i + " is " + k);
+		
+		i = -10;
+		k = i < 0 ? -i : i;
+		
+		System.out.print("Absolute value of ");
+		System.out.println(i + " is " + k);
+	}
+}
