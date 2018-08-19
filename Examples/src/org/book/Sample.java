a5fe806193d864076362ce6427e5f303408aa3b9
Omar Faroque <faroque.omar@outlook.com>
2018-08-19 08:51:27 -0400

    Sample Page 90

---
+++ Examples/src/org/book/Sample.java
@@ -0,0 +1,20 @@
+package org.book;
+/***
+ * page 90
+ * @author Admin
+ *
+ */
+public class Sample {
+
+	public static void main(String[] args) {
+		int a, b;
+		
+		b = 4;
+		
+		for(a=1; a<b; a++) {
+			System.out.println("a = " + a);
+			System.out.println("b = " + b);
+			b--;
+		}
+	}
+}
