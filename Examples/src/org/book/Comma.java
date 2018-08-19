287b0b54d8bab4351d09b3ab914988f64fd7ce72
Omar Faroque <faroque.omar@outlook.com>
2018-08-19 08:52:19 -0400

    Comma Page 90

---
+++ Examples/src/org/book/Comma.java
@@ -0,0 +1,17 @@
+package org.book;
+/***
+ * page 90
+ * @author Admin
+ *
+ */
+public class Comma {
+
+	public static void main(String[] args) {
+		int a, b;
+		
+		for(a=1, b=4; a<b; a++, b--) {
+			System.out.println("a = " + a);
+			System.out.println("b = " + b);
+		}
+	}
+}
