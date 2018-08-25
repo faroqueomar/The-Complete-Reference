e272a6d9ae0d1a972362393ea2e84c4a40a23067
Omar Faroque <faroque.omar@outlook.com>
2018-08-25 08:06:23 -0400

    BoxDemo Page 107

---
+++ Examples/src/org/book/BoxDemo.java
@@ -0,0 +1,25 @@
+package org.book;
+/***
+ * page 107
+ * @author Admin
+ *
+ */
+
+public class BoxDemo {
+
+	public static void main(String[] args) {
+		Box mybox = new Box();
+		double vol;
+		// assign values to mybox's instance variables
+		mybox.width = 10;
+		mybox.height = 20;
+		mybox.depth = 15;
+		// compute volume of box
+		vol = mybox.width * mybox.height * mybox.depth;
+		System.out.println("Volume is " + vol);
+	}
+
+}
+
+
+
