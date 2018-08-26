300f9ccb11c69501205a09a4622d71d7e0469d8c
Omar Faroque <faroque.omar@outlook.com>
2018-08-26 09:26:23 -0400

    Exc1 Page 207

---
+++ Examples/src/org/book/Exc1.java
@@ -0,0 +1,15 @@
+package org.book;
+/***
+ * page 207
+ * @author Admin
+ *
+ */
+public class Exc1 {
+	static void subroutine() {
+		int d = 0;
+		int a = 10 / d;
+	}
+	public static void main(String[] args) {
+		Exc1.subroutine();
+	}
+}
