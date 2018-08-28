57af5c2f27519378febe106a95e0fdde176bc30b
Omar Faroque <faroque.omar@outlook.com>
2018-08-28 17:37:41 -0400

    BoxPage 116-117

---
+++ Examples/src/Int_Classes_Box_Page116_117/Box.java
@@ -0,0 +1,25 @@
+package Int_Classes_Box_Page116_117;
+/***
+ * page 116-117
+ * @author Admin
+ *
+ */
+public class Box {
+	double width;
+	double height;
+	double depth;
+	
+	// compute and return volume
+	
+	double volume() {
+		return width * height * depth;
+	}
+	
+	// sets dimensions of box
+	
+	void setDim(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
+}
