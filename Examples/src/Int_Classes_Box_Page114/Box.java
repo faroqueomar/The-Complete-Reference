950b1f3b1601800b59353b25143f378feeb47b6d
Omar Faroque <faroque.omar@outlook.com>
2018-08-28 17:28:26 -0400

    Box Page 114

---
+++ Examples/src/Int_Classes_Box_Page114/Box.java
@@ -0,0 +1,16 @@
+package Int_Classes_Box_Page114;
+/***
+ * Page114
+ * @author Admin
+ *
+ */
+public class Box {
+	double width;
+	double height;
+	double depth;
+	
+	// compute and return volume
+	double volume() {
+		return width *height * depth;
+	}
+}
