9769258185c1d3051e311ac6382de97d6aeab374
Omar Faroque <faroque.omar@outlook.com>
2018-09-01 08:54:14 -0400

    Box Page 128

---
+++ Examples/src/Overloading_Constructors_Page128/Box.java
@@ -0,0 +1,25 @@
+package Overloading_Constructors_Page128;
+/***
+ * page 128
+ * @author Admin
+ *
+ */
+public class Box {
+	double width;
+	double height;
+	double depth;
+	
+	// This is the constructor for Box.
+	
+	Box(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
+	
+	// compute and return volume
+	
+	double volume() {
+		return width * height * depth;
+	}
+}
