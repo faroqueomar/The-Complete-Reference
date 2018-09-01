9084bbf570b0b1f8b0be19ac4851ed4e95ab1b97
Omar Faroque <faroque.omar@outlook.com>
2018-09-01 08:35:35 -0400

    Overload Page 126

---
+++ Examples/src/Overloading_Methods_Page125___/Overload.java
@@ -0,0 +1,19 @@
+package Overloading_Methods_Page125___;
+/***
+ * page 126
+ * @author Admin
+ *
+ */
+public class Overload {
+
+	public static void main(String[] args) {
+		OverloadDemo ob = new OverloadDemo();
+		double result;
+		// call all versions of test()
+		ob.test();
+		ob.test(10);
+		ob.test(10, 20);
+		result = ob.test(123.25);
+		System.out.println("Result of ob.test(123.25): " + result);
+	}
+}
