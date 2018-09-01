4cd3a75f789efb82501cae6a93c0534171679774
Omar Faroque <faroque.omar@outlook.com>
2018-09-01 08:54:32 -0400

    Box Page 129-130

---
+++ Examples/src/Overloading_Constructors_Page129_130/Box.java
@@ -0,0 +1,38 @@
+package Overloading_Constructors_Page129_130;
+/***
+ * page 129
+ * @author Admin
+ *
+ */
+public class Box {
+	double width;
+	double height;
+	double depth;
+	
+	// constructor used when all dimensions specified
+		
+	Box(double w, double h, double d) {
+		width = w;
+		height = h;
+		depth = d;
+	}
+	
+	// constructor used when no dimensions specified
+	Box() {
+		width = -1; // use -1 to indicate
+		height = -1; // an uninitialized
+		depth = -1; // box
+	}
+		
+	// constructor used when cube is created
+		
+	Box(double len) {
+		width = height = depth = len;
+	}
+	
+	// compute and return volume
+		
+	double volume() {
+		return width * height * depth;
+	}
+}
